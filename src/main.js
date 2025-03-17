075409c0ca9616e387e384a11963bef4e19df8a3
Pearlitox <151786530+Pearlitox@users.noreply.github.com>
2025-03-17 17:56:55 +0100

    style css+ ajout polices

--- src/main.js
+++ src/main.js
@@ -14,17 +14,15 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHe
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('.bg'),
 });
-
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
-
 camera.position.setZ(30);
-
 renderer.render( scene, camera );
+scene.background = new THREE.Color().setHex(0xe3e0ef);
 
 const texture = new THREE.TextureLoader().load('./matcap-iridescent.png' ); 
-
 const material = new THREE.MeshMatcapMaterial( { matcap:texture } );
+
 
 let model;
 
@@ -32,64 +30,30 @@ const loader = new GLTFLoader();
 loader.load('./star-web.gltf', function (gltf) {
   model = gltf.scene;
 
-  // Utilisation de l'Ia
   model.traverse((child) => {
     if (child.isMesh) {
       child.material = material;
     }
   });
-    // fin de l'utilisation de l'Ia
+
   model.scale.set(160,160,160)
   model.rotation.z = 120
   scene.add(model);
 
-  gsap.registerPlugin(ScrollTrigger);
-
-
-const tl = gsap.timeline({
-  scrollTrigger: {
-    trigger : '.landing',
-    start: 'top top',
-    end : '2000px',
-    scrub : true,
-    markers: true
-  }
-
-
-
-});
   
-    tl.to(model.position, {y: -2, z : 10, x:10})
- 
-    tl.to(model.position, {y: 0, z : 25, x:-3})
-
-    tl.to(model.position, {y: -1, z : 19, x:9})
-
-    tl.to(model.rotation, {x:-1}, "-=0.5")
-
-    tl.to(model.position, {y: 0, z : 0, x:0})
-
-    tl.to(model.rotation, {x:0}, "-=0.5")
-    
-
-
-
   animate(); // Lancer l'animation après chargement du modèle
 
 }, undefined, function (error) {
   console.error(error);
 });
-
-
-
-
 // Animation
 function animate() {
   requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+
   //model.rotation.y += 0.01;
   model.rotation.z += 0.01;
-  
+  renderer.render(scene, camera);
 }
+
 
 animate();
